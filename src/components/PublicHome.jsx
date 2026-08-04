3df8535d9fc70b0f1dca765870275a4b462ada53
codeinfinite19 <shahedhassan572@gmail.com>
2026-08-05 02:21:35 +0600

    updated lelel live

--- src/components/PublicHome.jsx
+++ src/components/PublicHome.jsx
@@ -68,7 +68,7 @@ export default function PublicHome({ session }) {
       <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
       <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
     </span>
-    LIVE
+    Attention
   </div>
 
   {/* Marquee */}
